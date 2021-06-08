0287184fc699b307642e382931079eb02d4d0409
rafaelNandes <rafael.fernandessilva@hotmail.com>
2021-06-07 21:49:53 -0300

    create recycleview

--- app/src/main/java/com/example/api/MainActivity.kt
+++ app/src/main/java/com/example/api/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.api
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerProducts: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerProducts = findViewById(R.id.recycler_products)
     }
 }
